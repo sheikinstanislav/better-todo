705036c7bd9d0c00cf8498b2743eadcb55360ffa
sheikinstanislav <sheikinstanislav@gmail.com>
2022-10-17 22:10:48 +0300

    add tailwind and create layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import './header.scss';
+
+export const Header: FC = () => {
+  return (
+    <header className="header bg-slate-700 text-yellow-50">
+      <div className="container">header</div>
+    </header>
+  );
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      padding: '2rem',
+      center: true
+    },
+    extend: {}
+  },
+  plugins: []
+};
